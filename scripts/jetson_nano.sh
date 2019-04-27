97a87ceadd214681bb4f8550b9fc0bd643dd23b1
F5OEO <evaristec@gmail.com>
2019-04-27 13:50:53 +0200

    Remove buffer on nano,modify bitrate calculation

--- scripts/jetson_nano.sh
+++ scripts/jetson_nano.sh
@@ -9,10 +9,10 @@ CALL=F5OEO
 # 1/4,1/3,2/5,1/2,3/5,2/3,3/4,4/5,5/6,8/9,9/10 for DVB-S2 QPSK.
 # 3/5,2/3,3/4,5/6,8/9,9/10 for DVB-S2 8PSK
 source ./include/modulateparam.sh
-FREQ=2403
-SYMBOLRATE=250
-FECNUM=1
-FECDEN=2
+FREQ=2100
+SYMBOLRATE=125
+FECNUM=5
+FECDEN=6
 #DVBS,DVBS2
 MODE=DVBS2
 #QPSK,8PSK,16APSK,32APSK
@@ -23,11 +23,11 @@ TYPE_FRAME=$LONG_FRAME
 # $WITH_PILOTS,WITHOUT_PILOTS
 PILOTS=$WITHOUT_PILOTS
 # Upsample 1,2 or 4 : 4 delivers the best quality but should not be up to 500KS
-UPSAMPLE=2
+UPSAMPLE=1
 
 # ------- ENCODER PARAMETERS --------
 
-VIDEO_RESX=360
+VIDEO_RESX=352
 # 16:9 or 4:3
 RATIO=16:9
 case "$RATIO" in
@@ -38,7 +38,7 @@ let VIDEO_RESY=VIDEO_RESX*3/4 ;;
 esac
 
 #Uncomment if don't want to use ratio calculation
-#VIDEO_RESY=360
+#VIDEO_RESY=1080
 
 
 #Only 25 is working well with audio
@@ -49,12 +49,12 @@ VIDEO_GOP=100
 #VIDEO INPUT
 
 #Could be  VIDEOSOURCE_PICAMERA, VIDEOSOURCE_USB_CAM , VIDEOSOURCE_IP
-VIDEOSOURCE=VIDEOSOURCE_PICAMERA
+VIDEOSOURCE=VIDEOSOURCE_IP
 VIDEOSOURCE_IP_ADRESS=230.0.0.1
 VIDEOSOURCE_IP_PORT=5004
 
 # H264 or H265
-CODEC=H265
+CODEC=H264
 
 #AUDIO INPUT
 
@@ -65,7 +65,7 @@ AUDIO_BITRATE=20000
 
 # Bitrate
 source ./include/getbitrate.sh
-let TS_AUDIO_BITRATE=AUDIO_BITRATE*12/10
+let TS_AUDIO_BITRATE=AUDIO_BITRATE*14/10
 let VIDEOBITRATE=(BITRATE_TS-12000-TS_AUDIO_BITRATE)*650/1000
 let VIDEOPEAKBITRATE=VIDEOBITRATE*110/100
 #OUTPUT TYPE LIME or IP
@@ -78,7 +78,7 @@ echo VideoPeakBitrate = $VIDEOPEAKBITRATE
 
 case "$OUTPUT" in
 "LIME")
-source ./include/nanoencode.sh |buffer| source ./include/limerf.sh
+source ./include/nanoencode.sh | source ./include/limerf.sh
 ;;
 "IP")
 source ./include/nanoencode.sh
