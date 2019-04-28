4e12c03f2934300ee446e27176430c8ee35c3dc9
F5OEO <evaristec@gmail.com>
2019-04-28 16:28:03 +0200

    Remove sudo for Jetson

--- scripts/include/limerf.sh
+++ scripts/include/limerf.sh
@@ -1,2 +1,5 @@
-sudo ../bin/limesdr_dvb  -s "$SYMBOLRATE"000 -f $FECNUM/$FECDEN  -r $UPSAMPLE -m $MODE -c $CONSTELLATION $TYPE_FRAME $PILOTS -t "$FREQ"e6 -g $GAIN -q 0
-
+if  [ "$Platform" = "rpi" ] ; then
+    sudo ../bin/limesdr_dvb  -s "$SYMBOLRATE"000 -f $FECNUM/$FECDEN  -r $UPSAMPLE -m $MODE -c $CONSTELLATION $TYPE_FRAME $PILOTS -t "$FREQ"e6 -g $GAIN -q 0
+else
+    ../bin/limesdr_dvb  -s "$SYMBOLRATE"000 -f $FECNUM/$FECDEN  -r $UPSAMPLE -m $MODE -c $CONSTELLATION $TYPE_FRAME $PILOTS -t "$FREQ"e6 -g $GAIN -q 0
+fi
